dbba2996a52920c1032b51944824468229b2d895
rezamoridi <rezamoridi2042@gmail.com>
2024-05-04 01:12:36 +0330

    beta

--- test/test.py
+++ test/test.py
@@ -1,22 +1,8 @@
-def check_code_meli(code):
-    code1 = str(code)
-    L = len(code1)
-    
-    if L < 8 or int(code) == 0:
-        return False
-    
-    code1 = ('0000' + code1)[-10:]
-    
-    if int(code1[3:9]) == 0:
-        return False
-    
-    c = int(code1[9])
-    s = 0
-    for i in range(9):
-        s += int(code1[i]) * (10 - i)
-    
-    s = s % 11
-    
-    return (s < 2 and c == s) or (s >= 2 and c == (11 - s))
+import datetime
 
-print(check_code_meli(5929451850))
+
+datetime.MAXYEAR = 1403
+
+a = "1402-04-04"
+
+print(isinstance(a, datetime.datetime))
